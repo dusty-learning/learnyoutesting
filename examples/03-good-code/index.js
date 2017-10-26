f9d278287cf51d320a523c1214505bb2dd34b7d8
dhershman <dhershman@dealertire.com>
2017-10-26 17:29:17 -0400

    Added Example 3 content

---
+++ examples/03-good-code/index.js
@@ -0,0 +1,62 @@
+// So let's take a gander at a nicely testable library of code
+
+var extend = function () {
+  var args = Array.prototype.slice.call(arguments);
+
+  return args.reduce((acc, x) => {
+    let key = '';
+
+    for (key in x) {
+      acc[key] = x[key];
+    }
+
+    return acc;
+  }, {});
+};
+
+var validationLibrary = function (options) {
+  var defaults = {
+    max: 20,
+    min: 0
+  };
+  // Override our defaults with options that are passed in
+  var opts = extend(defaults, options);
+
+  var isNumber = function (val) {
+    return !isNaN(val);
+  };
+
+  // Some crude logic validation
+  var validateOptions = function () {
+    if (!isNumber(opts.max)) {
+      opts.max = defaults.max;
+    }
+
+    if (!isNumber(opts.min)) {
+      opts.min = defaults.min;
+    }
+  };
+
+  var aboveMin = function (val) {
+    return isNumber(val) && Number(val) > opts.min;
+  };
+
+  var belowMax = function (val) {
+    return isNumber(val) && Number(val) < opts.max;
+  };
+
+  var withinLimit = function (val) {
+    return isNumber(val) && Number(val) < opts.max && Number(val) > opts.min;
+  };
+
+  validateOptions();
+
+  return {
+    isNumber: isNumber,
+    aboveMin: aboveMin,
+    belowMax: belowMax,
+    withinLimit: withinLimit
+  };
+}
+
+module.exports = validationLibrary;
---
+++ examples/03-good-code/tests.js
@@ -0,0 +1,85 @@
+var test = require('tape');
+var validationLib = require('./index');
+
+test('Test Default Options', function (t) {
+  var validate = validationLib();
+
+  t.notOk(validate.withinLimit(21), '21 is greater than the default of 20');
+  t.notOk(validate.withinLimit(-1), '-1 is less than the default of 0');
+
+  t.end();
+});
+
+test('Test Option Validation', function (t) {
+  var validate = validationLib({
+    max: 'ABC',
+    min: 'DEF'
+  });
+
+  // They should reset to their defaults of 20 and 0
+  t.notOk(validate.withinLimit(21), '21 is greater than the default of 20');
+  t.notOk(validate.withinLimit(-1), '-1 is less than the default of 0');
+
+  t.end();
+});
+
+test('aboveMin', function (t) {
+  // Create our new instance
+  // We will just stick to our defaults for now
+  var validate = validationLib();
+
+  t.ok(validate.aboveMin(3), '3 is greater than 0');
+  t.ok(validate.aboveMin('3'), 'String 3 is greater than 0');
+  t.notOk(validate.aboveMin(0), '0 is not greater than 0');
+  t.notOk(validate.aboveMin('ABC'), 'ABC is not greater than 0');
+
+  t.end();
+});
+
+test('belowMax', function (t) {
+  // Create our new instance
+  // We will just stick to our defaults for now
+  var validate = validationLib();
+
+  t.ok(validate.belowMax(3), '3 is below 20');
+  t.ok(validate.belowMax('3'), 'String 3 is below 20');
+  t.notOk(validate.belowMax(20), '20 is not below than 20');
+  t.notOk(validate.belowMax('ABC'), 'ABC is not below than 20');
+
+  t.end();
+});
+
+test('Test withinLimit', function (t) {
+  // You don't need to do this everytime, you can set validate at the top as well and use that
+  // This just keeps it to a brand new instance per test block
+  var validate = validationLib();
+
+  t.ok(validate.withinLimit(4), '4 is within or max and min');
+  t.ok(validate.withinLimit(8), '8 is within or max and min');
+  t.ok(validate.withinLimit('7'), '7 is within or max and min');
+  t.notOk(validate.withinLimit(20), '20 is not within or max and min');
+  t.notOk(validate.withinLimit(-2), '-2 is not within or max and min');
+
+  t.end();
+});
+
+test('Run tests with custom options', function (t) {
+  var validate = validationLib({
+    max: 10,
+    min: 5
+  });
+  // Let's set our function to a variable for easier access
+  var aboveMin = validate.aboveMin;
+  var belowMax = validate.belowMax;
+
+  t.ok(aboveMin(6), '6 is above 5');
+  t.ok(aboveMin(10), '10 is above 5');
+  t.notOk(aboveMin(4), '4 is not above 5');
+  t.notOk(aboveMin(5), '5 is not above 5');
+  t.ok(belowMax(6), '6 is below 10');
+  t.ok(belowMax(4), '4 is below 10');
+  t.notOk(belowMax(10), '10 is not below 10');
+  t.notOk(belowMax(11), '1 is not below 10');
+
+  t.end();
+});
